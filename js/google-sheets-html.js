d58053ad1a8faf39190e552c058b352919469469
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-22 15:15:42 +0530

    pkMasterMoves round1 leaderboard has been updated

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -111,12 +111,12 @@ function rowsCount(item){
   dataList.forEach(dataCount);
   console.log("counter is"+counter);
 
-  if(counter>=0 && counter<=8){
+  if(counter>=0 && counter<=12){
 $('#conDiv').append('<div class="colThree"><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
 
   }
 
-  else  if(counter>8 && counter<=14){
+  else  if(counter>12 && counter<=22){
 $('#conDivTalent').append('<div class="colThree"><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
   }
 
